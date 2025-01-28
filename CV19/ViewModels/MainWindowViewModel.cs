70c957bb77436647bedaa8ab6218765c8984ab9f
Юрий Копченов <kopchenovuv@mail.ru>
2025-01-28 20:59:44 +0500

    Тестирование работы модели представления

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Анализ статистики CV19";
+
+        public string Title
+        {
+            get => _Title; 
+            set => Set(ref _Title, value);
+        }
+    }
+}
