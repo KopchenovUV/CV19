0bf8163471caf315d768f422e2facf33fbe9ef7c
Юрий Копченов <kopchenovuv@mail.ru>
2025-01-29 15:35:45 +0500

    Добавляем в модель представления Главного окна команду выхода из приложения

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CV19.Infrastructure.Comands;
 using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
         private string _Title = "Анализ статистики CV19";
 
         public string Title
@@ -16,15 +20,38 @@ namespace CV19.ViewModels
             get => _Title; 
             set => Set(ref _Title, value);
         }
+        #endregion
 
-
-
+        #region Status : string - Статус программы
         private string _Status = "Готов!";
 
         public string Status
         {
             get => _Status;
             set => Set(ref _Status, value);
+        }
+        #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p) 
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+
+            #endregion
         }
     }
 }
