25a05acc35c7a93af2a05765f59f516c9ff2f278
Юрий Копченов <kopchenovuv@mail.ru>
2025-01-29 15:46:41 +0500

    Определение класс отдельной команды

---
+++ CV19/Infrastructure/Comands/CloseApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CV19.Infrastructure.Comands.Base;
+
+namespace CV19.Infrastructure.Comands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
