d67cd51916b4e85a58f9bf6fb33bcd082a70daf5
Юрий Копченов <kopchenovuv@mail.ru>
2025-01-30 18:45:01 +0500

    Запрос к удалённому серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,17 @@ namespace CV19Console
 {
     internal class Program
     {
+        const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/refs/heads/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
+            //WebClient client = new WebClient();
+
+            var client = new HttpClient();
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
